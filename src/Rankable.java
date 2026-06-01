0dae8d3a4cd94c2d74781ec81349440bddd8ad56
cookiemintchoc <ninasyukkie@gmail.com>
2026-06-01 08:58:56 +0800

    Update Rankable.java

--- src/Rankable.java
+++ src/Rankable.java
@@ -1,3 +1,12 @@
-public class Rankable {
+public interface Rankable {
+//Memmber 4
+// Made by Nur Syukrinah binti Suhaidi (97717)
+    
+    // Returns the current rank of a player.
+    String getRank();
+    // Returns the player's points or score.
+    int getPoints();
+    // Updates the player's rank based on points.
+    void updateRank();
     
 }
