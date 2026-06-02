ad0646fb712d0847bc226226d93e16479daba708
cookiemintchoc <ninasyukkie@gmail.com>
2026-06-03 01:41:06 +0800

    badgemanager update

--- src/BadgeManager.java
+++ src/BadgeManager.java
@@ -1,3 +1,14 @@
 public class BadgeManager {
+    //Member 4
+    //Made by Nur Syukrinah binti Suhaidi (97717)
+    
+    // This method takes the player's score as input
+    // and returns the corresponding badge (rank).
+    public String getBadge(int score) {
+        if (score >= 90) return "Master";
+        else if (score >= 75) return "Expert";
+        else if (score >= 50) return "Intermediate";
+        else return "Beginner";
+    }
     
 }
